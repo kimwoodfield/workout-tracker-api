21483f2b5349444756672823b1d019d0932a0149
kimwoodfield <kimwoodfield@live.com>
2021-03-10 16:11:08 +1000

    databased pool working

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,30 @@
 const mysql = require('mysql');
 
 // Create pool - allows us to query pool instead of db directly
-mysql.createPool({
+const pool = mysql.createPool({
     connectionLimit: 10,
-    password: '',
-    user: 'root',
+    password: 'password1',
+    user: 'Kim',
     database: 'workout_tracker',
     host: 'localhost',
     port: '8889'
 });
 
-// Create an object for the database
-let workout_tracker_db = {};
+
+let workout_trackerdb = {};
+
+// Function that returns all users in the database
+workout_trackerdb.all = () => {
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT * FROM `User`', (err, results) => {
+            // If there's an error, reject this promise
+            if (err) {
+                return reject(err);
+            }
+            // Otherwise return our results
+            return resolve(results);
+        });
+    });
+};
+
+module.exports = workout_trackerdb;
