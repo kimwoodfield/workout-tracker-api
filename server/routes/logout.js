71ffb25bc6076f93610442822e5412ddb280f6f8
Kim Woodfield <kimwoodfield@live.com>
2021-05-02 16:28:50 +1000

    successful logout returns 201

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -9,11 +9,20 @@ const router = express.Router();
 // If a user accesses this route, log them out of the application.
 router.post("/", (req ,res) => {
 
+    console.log('made it to logout route');
+
     // Store IP from Req obj and UserType for logging
     let ip = req.ip;
+
+    console.log('req.session is ', req.session);
+    console.log('req.session.userType is ', req.session.userType);
+    console.log('req.session.userType.userRole is ', req.session.userType.userRole);
     let userType = req.session.userType.userRole;
 
+    console.log('req.session.userID is ', req.session.userID);
+
     if (!req.session.userID) {
+        console.log('log at the start of first if statement');
         logger.info(`Validation was unsuccessful on attempted logout. Attempted access from an unauthorized user located at IP address: ${ip}`);
         return res.status(403).json({
             status: 403,
@@ -21,10 +30,13 @@ router.post("/", (req ,res) => {
         });
     }
     else {
+        console.log('made it past the first if statement');
         logger.info(`Successfully logged out the current user. The user was logged in as userType: ${userType} from IP address: ${ip}`);
         res.clearCookie('User cookie');
-        return res.status(200).json({
-            status: 200,
+        console.log('the usertype is ', userType, ' and the ip is ', ip);
+        console.log('made it to the return');
+        return res.status(201).json({
+            status: 201,
             msg: 'Session has been destroyed'
         });
     }
