6f760eea7052e2f93960e29f025255c066e02da4
kimwoodfield <kimwoodfield@live.com>
2021-05-18 17:55:35 +1000

    upaded sql file

--- workout_tracker.sql
+++ workout_tracker.sql
@@ -1,22 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.9.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: May 18, 2021 at 03:26 AM
--- Server version: 8.0.18
--- PHP Version: 7.3.12
+-- Host: localhost:8889
+-- Generation Time: May 18, 2021 at 07:54 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `workout_tracker`
@@ -25,107 +17,188 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `exercise`
+-- Table structure for table `Exercise`
 --
 
-DROP TABLE IF EXISTS `exercise`;
-CREATE TABLE IF NOT EXISTS `exercise` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `exercise_name` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exercise_type` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `Exercise` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `exercise_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exercise_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `routine`
+-- Table structure for table `Routine`
 --
 
-DROP TABLE IF EXISTS `routine`;
-CREATE TABLE IF NOT EXISTS `routine` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `routine_name` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `Routine` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `routine_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `routineexercise`
+-- Table structure for table `RoutineExercise`
 --
 
-DROP TABLE IF EXISTS `routineexercise`;
-CREATE TABLE IF NOT EXISTS `routineexercise` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `RoutineExercise` (
+  `id` int(10) UNSIGNED NOT NULL,
   `routine_id` int(10) UNSIGNED NOT NULL,
-  `exercise_id` int(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_routine_id` (`routine_id`),
-  KEY `fk_exercise_id` (`exercise_id`)
+  `exercise_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `User`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `email` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `full_name` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `User` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `email` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `full_name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `workout`
+-- Table structure for table `Workout`
 --
 
-DROP TABLE IF EXISTS `workout`;
-CREATE TABLE IF NOT EXISTS `workout` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `Workout` (
+  `id` int(10) UNSIGNED NOT NULL,
   `workout_date` datetime NOT NULL,
   `routine_id` int(10) UNSIGNED NOT NULL,
-  `user_id` int(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`)
+  `user_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `workoutexercise`
+-- Table structure for table `WorkoutExercise`
 --
 
-DROP TABLE IF EXISTS `workoutexercise`;
-CREATE TABLE IF NOT EXISTS `workoutexercise` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `WorkoutExercise` (
+  `id` int(10) UNSIGNED NOT NULL,
   `workout_id` int(10) UNSIGNED NOT NULL,
   `exercise_id` int(10) UNSIGNED NOT NULL,
-  `sets` int(10) NOT NULL,
-  `weight` float NOT NULL,
-  `reps` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
+  `sets` int(4) NOT NULL,
+  `weight` int(6) NOT NULL,
+  `reps` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Exercise`
+--
+ALTER TABLE `Exercise`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Routine`
+--
+ALTER TABLE `Routine`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `RoutineExercise`
+--
+ALTER TABLE `RoutineExercise`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_exerciseID` (`exercise_id`),
+  ADD KEY `fk_routineID` (`routine_id`);
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Workout`
+--
+ALTER TABLE `Workout`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_routine_id` (`routine_id`),
+  ADD KEY `fk_user_id` (`user_id`);
+
+--
+-- Indexes for table `WorkoutExercise`
+--
+ALTER TABLE `WorkoutExercise`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_workout_id` (`workout_id`),
+  ADD KEY `fk_exercise_id` (`exercise_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Exercise`
+--
+ALTER TABLE `Exercise`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Routine`
+--
+ALTER TABLE `Routine`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `RoutineExercise`
+--
+ALTER TABLE `RoutineExercise`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `User`
+--
+ALTER TABLE `User`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Workout`
+--
+ALTER TABLE `Workout`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `WorkoutExercise`
+--
+ALTER TABLE `WorkoutExercise`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `routineexercise`
+-- Constraints for table `RoutineExercise`
 --
-ALTER TABLE `routineexercise`
-  ADD CONSTRAINT `fk_exercise_id` FOREIGN KEY (`exercise_id`) REFERENCES `exercise` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_routine_id` FOREIGN KEY (`routine_id`) REFERENCES `routine` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
+ALTER TABLE `RoutineExercise`
+  ADD CONSTRAINT `fk_exerciseID` FOREIGN KEY (`exercise_id`) REFERENCES `Exercise` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_routineID` FOREIGN KEY (`routine_id`) REFERENCES `Routine` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Constraints for table `Workout`
+--
+ALTER TABLE `Workout`
+  ADD CONSTRAINT `fk_routine_id` FOREIGN KEY (`routine_id`) REFERENCES `Routine` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `WorkoutExercise`
+--
+ALTER TABLE `WorkoutExercise`
+  ADD CONSTRAINT `fk_exercise_id` FOREIGN KEY (`exercise_id`) REFERENCES `Exercise` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_workout_id` FOREIGN KEY (`workout_id`) REFERENCES `Workout` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
