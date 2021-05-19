7067bd710f8bb129705a0725e0b144ba63161503
kimwoodfield <kimwoodfield@live.com>
2021-05-19 10:40:32 +1000

    added ::1 to list of whitelisted ip addresses

--- server/routes/isAdmin.js
+++ server/routes/isAdmin.js
@@ -10,7 +10,7 @@ const router = express.Router();
 // Here we check what userType is set on the Session. If the userType is undefined, it means that the user attempted to access the route without creating an account first. If a user attempts to access the route with a userType of "General" then they are considered unauthorized to access the admin panel and a 403 response code is returned to the client as a result.
 router.get("/", (req, res) => {
   // Store IP from Req obj and UserType for logging
-  let whitelistedIP = ["138.44.128.241", "::ffff:127.0.0.1"];
+  let whitelistedIP = ["138.44.128.241", "::ffff:127.0.0.1", "::1"];
 
   let ip = req.ip;
 
@@ -43,7 +43,6 @@ router.get("/", (req, res) => {
     });
   } else {
     // Compare IP with Whitelisted IP
-    console.log(ip === whitelistedIP[1]);
     if (whitelistedIP.indexOf(ip) === -1) {
       console.log(`The IP's match`);
       res.sendStatus(403);
